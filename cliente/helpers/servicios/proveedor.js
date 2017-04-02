506a9bd8911c9ff078e7d9addb7555ca1d320550
vrinox <vrinox@hotmail.com>
2017-04-02 16:55:52 -0300

    '1'

---
+++ cliente/helpers/servicios/proveedor.js
@@ -0,0 +1,6 @@
+angular.module('balaFriaVentas')
+.factory('Proveedores', ['$resource', function($resource){
+  return $resource('/proveedor/:id', null, {
+    'update': { method:'PUT' }
+  });
+}]);
