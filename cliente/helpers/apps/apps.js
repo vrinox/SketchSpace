5bf6368c4894369bec8ce0a5570be3f223473d4b
vleon <vleon@socaportuguesa.com>
2017-04-04 15:46:36 -0400

    mejora plantilla registro

--- cliente/helpers/apps/apps.js
+++ cliente/helpers/apps/apps.js
@@ -1,8 +1,12 @@
 angular.module('sketch', ['ngMaterial','leaflet-directive','ngRoute', 'ngResource'])
-.config(['$routeProvider', function ($routeProvider) {
+.config(['$routeProvider','$mdThemingProvider', function ($routeProvider,$mdThemingProvider) {
   $routeProvider
     .when('/', {
       templateUrl: '/views/plantillas/landing.html',
       controller: 'ctrlLanding'
     });
+    $mdThemingProvider.theme('default')
+          .primaryPalette('cyan')
+          .accentPalette('blue-grey')
+          .dark();
 }]);
